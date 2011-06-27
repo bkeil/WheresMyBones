846cde2f02625df5a17e5d5295ea7c79daadb794
bkeil <bfkeil@gmail.com>
2011-06-26 19:22:57 -0700

    Cut the chatter on player death.

--- WheresMyBones/src/com/mouthpunch/wheresmybones/WheresMyBonesEntityListener.java
+++ WheresMyBones/src/com/mouthpunch/wheresmybones/WheresMyBonesEntityListener.java
@@ -1,8 +1,6 @@
 package com.mouthpunch.wheresmybones;
 
-import org.bukkit.Location;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityListener;
@@ -17,25 +15,10 @@ public class WheresMyBonesEntityListener extends EntityListener {
 
 	@Override
 	public void onEntityDeath(EntityDeathEvent event) {
-
 		final Entity entity = event.getEntity();
-		final Location location = entity.getLocation();
-
-		final String name;
-		if (entity instanceof HumanEntity) {
-			name = ((HumanEntity) entity).getName();
-		} else {
-			name = entity.getClass().getSimpleName();
-		}
-
 		if (entity instanceof Player) {
 			plugin.storeBonesLocation(((Player) entity).getName(),
 					entity.getLocation());
 		}
-
-		plugin.getServer().broadcastMessage(
-				name + " died in " + location.getWorld().getName() + " at ("
-						+ location.getBlockX() + ", " + location.getBlockY()
-						+ ", " + location.getBlockZ() + ")");
 	}
 }
