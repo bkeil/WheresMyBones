23dec94d79cd0ff7570db4cfba072c35f420daed
bkeil <bfkeil@gmail.com>
2011-07-08 08:58:59 -0700

    Added ability to set compass and view orientation towards a particular set of bones.  Updated version to 0.0.2.

--- WheresMyBones/src/com/mouthpunch/wheresmybones/MyBonesCommand.java
+++ WheresMyBones/src/com/mouthpunch/wheresmybones/MyBonesCommand.java
@@ -1,12 +1,15 @@
 package com.mouthpunch.wheresmybones;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class MyBonesCommand implements CommandExecutor {
 
@@ -25,8 +28,18 @@ public class MyBonesCommand implements CommandExecutor {
 
 		final Player player = (Player) sender;
 
-		Iterator<Location> i = plugin.loadBonesLocations(player.getName()).iterator();
+		Iterator<Location> i = plugin.loadBonesLocations(player.getName())
+				.iterator();
 
+		if (args.length == 0) {
+			listBones(player, i);
+			return true;
+		} else {
+			return orientPlayer(player, args, i);
+		}
+	}
+
+	private void listBones(Player sender, Iterator<Location> i) {
 		if (i.hasNext()) {
 			int j = 1;
 			sender.sendMessage("Your bones:");
@@ -39,6 +52,59 @@ public class MyBonesCommand implements CommandExecutor {
 		} else {
 			sender.sendMessage("Your bones are lost.");
 		}
+	}
+
+	private boolean orientPlayer(Player player, String[] args,
+			Iterator<Location> it) {
+		final int bone;
+		try {
+			bone = Integer.parseInt(args[0], 10);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
+		if (bone == 0) {
+			player.sendMessage("There is no bone 0.");
+			return true;
+		}
+
+		int i = 0;
+		Location loc = null;
+		while (i < bone) {
+			if (it.hasNext()) {
+				loc = it.next();
+				++i;
+			} else {
+				player.sendMessage("I only know about " + i + " bone"
+						+ (i == 1 ? "" : "s") + " for you");
+				return true;
+			}
+		}
+
+		loc = loc.clone();
+
+		List<String> argsList = Arrays.asList(args);
+		player.sendMessage(args.toString());
+
+		if (argsList.indexOf("compass") != -1) {
+			player.setCompassTarget(loc);
+		}
+
+		final Vector n = loc.subtract(player.getEyeLocation()).toVector()
+				.normalize();
+
+		float pitch = (float) Math.toDegrees(Math.asin(n.getY()));
+		pitch = -pitch;
+
+		float yaw = (float) Math.toDegrees(Math.atan2(n.getZ(), n.getX()));
+		yaw -= 90f;
+
+		final Location playerLoc = player.getLocation();
+		playerLoc.setPitch(pitch);
+		playerLoc.setYaw(yaw);
+
+		player.teleport(playerLoc);
+
 		return true;
 	}
 }
