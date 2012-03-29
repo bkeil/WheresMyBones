1d53d8a277240f508b0cf8ee6783d2f824890dfc
bkeil <bfkeil@gmail.com>
2012-03-28 20:34:30 -0700

    Updates for Bukkit 1.2.4

--- WheresMyBones/src/com/mouthpunch/wheresmybones/WheresMyBones.java
+++ WheresMyBones/src/com/mouthpunch/wheresmybones/WheresMyBones.java
@@ -5,22 +5,15 @@ import java.util.Map;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.nijiko.permissions.PermissionHandler;
-import com.nijikokun.bukkit.Permissions.Permissions;
-
 public class WheresMyBones extends JavaPlugin {
 
-	private volatile PermissionHandler permissionHandler;
-
-	private final Map<String, Locations> store = new HashMap<String, Locations>();
+private final Map<String, Locations> store = new HashMap<String, Locations>();
 
 	public boolean checkPermission(Player player, String permission) {
-		return (permissionHandler == null) ? player.isOp() : permissionHandler
-				.has(player, permission);
+		return player.hasPermission(permission);
 	}
 
 	public Locations loadBonesLocations(String playerName) {
@@ -40,8 +33,6 @@ public class WheresMyBones extends JavaPlugin {
 		PluginDescriptionFile pdfFile = this.getDescription();
 		System.out.println(pdfFile.getName() + " version "
 				+ pdfFile.getVersion() + " is enabled!");
-
-		setUpPermissions();
 	}
 
 	public void storeBonesLocation(String playerName, Location location) {
@@ -55,24 +46,5 @@ public class WheresMyBones extends JavaPlugin {
 			store.put(playerName, locations);
 		}
 		return locations;
-	}
-
-	private void setUpPermissions() {
-		if (permissionHandler != null) {
-			return;
-		}
-
-		Plugin permissionsPlugin = this.getServer().getPluginManager()
-				.getPlugin("Permissions");
-
-		if (permissionsPlugin == null) {
-			System.out
-					.println("Permission system not detected, defaulting to OP");
-			return;
-		}
-
-		permissionHandler = ((Permissions) permissionsPlugin).getHandler();
-		System.out.println("Found and will use plugin "
-				+ permissionsPlugin.getDescription().getFullName());
 	}
 }
